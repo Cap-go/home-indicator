951193674e92dce2f627da5793cce2ea740e2f6d
Martin Donadieu <martindonadieu@gmail.com>
2023-07-04 17:19:10 +0100

    fix: indicator code

--- ios/Plugin/HomeIndicatorPlugin.swift
+++ ios/Plugin/HomeIndicatorPlugin.swift
@@ -1,32 +1,48 @@
 import Foundation
 import Capacitor
 
+
+extension CAPBridgeViewController{
+    
+    private struct Holder {
+        static var _setHomeIndicator: Bool = false
+    }
+    
+    public func hideHomeIndicator(_ ishomeIndicatorVisible: Bool){
+        
+        Holder._setHomeIndicator=ishomeIndicatorVisible
+        self.setNeedsUpdateOfHomeIndicatorAutoHidden()
+    }
+    
+    
+    override public var prefersHomeIndicatorAutoHidden: Bool {
+
+        return Holder._setHomeIndicator
+        
+    }
+}
+
+
 @objc(HomeIndicatorPlugin)
 public class HomeIndicatorPlugin: CAPPlugin {
 
     @objc func hide(_ call: CAPPluginCall) {
-        DispatchQueue.main.async {
-            let controller = UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.rootViewController
-            if var appDelegateRootController = controller {
-                appDelegateRootController.setNeedsUpdateOfHomeIndicatorAutoHidden()
-                appDelegateRootController.prefersHomeIndicatorAutoHidden = true 
-                call.resolve()
-            } else {
-                call.reject("Could not hide home indicator")
+        guard let bridgeVC = self.bridge?.viewController as? CAPBridgeViewController else {
+                call.reject("")
+                return
             }
+        DispatchQueue.main.async {
+            bridgeVC.hideHomeIndicator(true)
         }
     }
 
     @objc func show(_ call: CAPPluginCall) {
-        DispatchQueue.main.async {
-            let controller = UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.rootViewController
-            if var appDelegateRootController = controller {
-                appDelegateRootController.setNeedsUpdateOfHomeIndicatorAutoHidden()
-                appDelegateRootController.prefersHomeIndicatorAutoHidden = false
-                call.resolve()
-            } else {
-                call.reject("Could not show home indicator")
+        guard let bridgeVC = self.bridge?.viewController as? CAPBridgeViewController else {
+                call.reject("")
+                return
             }
+        DispatchQueue.main.async {
+            bridgeVC.hideHomeIndicator(false)
         }
     }
 }
