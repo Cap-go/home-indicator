ed540574326b778fb0eb4e463f612358a62056c4
Martin Donadieu <martindonadieu@gmail.com>
2023-08-18 04:29:59 +0200

    fix: set vars for indicators

--- android/src/main/java/ee/forgr/home_indicator/HomeIndicatorPlugin.java
+++ android/src/main/java/ee/forgr/home_indicator/HomeIndicatorPlugin.java
@@ -10,6 +10,33 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 @CapacitorPlugin(name = "HomeIndicator")
 public class HomeIndicatorPlugin extends Plugin {
 
+
+    private void setCssVar() {
+        WindowInsets windowInsets = MainActivity.this.getBridge().getActivity().getWindow().getDecorView().getRootWindowInsets();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && windowInsets != null) {
+            var res = windowInsets.getInsets(WindowInsets.Type.systemBars());
+            float density = MainActivity.this.getBridge().getActivity().getResources().getDisplayMetrics().density;
+            getBridge().getWebView().evaluateJavascript("document.documentElement.style.setProperty('--safe-area-inset-bottom', '" + res.bottom / density + "px');", null);
+            getBridge().getWebView().evaluateJavascript("document.documentElement.style.setProperty('--safe-area-inset-top', '" + res.top / density + "px');", null);
+            getBridge().getWebView().evaluateJavascript("document.documentElement.style.setProperty('--safe-area-inset-right', '" + res.right / density + "px');", null);
+            getBridge().getWebView().evaluateJavascript("document.documentElement.style.setProperty('--safe-area-inset-left', '" + res.left / density + "px');", null);
+        }
+    }
+
+    @Override
+    public void load() {
+        super.load();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // start task after 500ms in android
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    HomeIndicatorPlugin.this.setCssVar();
+                }
+            }, 500);
+        }
+    }
+
     @PluginMethod
     public void hide(PluginCall call) {
         getActivity()
@@ -20,6 +47,7 @@ public class HomeIndicatorPlugin extends Plugin {
                         View decorView = getActivity().getWindow().getDecorView();
                         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
                         decorView.setSystemUiVisibility(uiOptions);
+                        HomeIndicatorPlugin.this.setCssVar();
                         JSObject ret = new JSObject();
                         call.resolve(ret);
                     }
@@ -37,6 +65,7 @@ public class HomeIndicatorPlugin extends Plugin {
                         View decorView = getActivity().getWindow().getDecorView();
                         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
                         decorView.setSystemUiVisibility(uiOptions);
+                        HomeIndicatorPlugin.this.setCssVar();
                         JSObject ret = new JSObject();
                         call.resolve(ret);
                     }
@@ -53,6 +82,7 @@ public class HomeIndicatorPlugin extends Plugin {
                     public void run() {
                         View decorView = getActivity().getWindow().getDecorView();
                         int uiOptions = decorView.getSystemUiVisibility();
+                        HomeIndicatorPlugin.this.setCssVar();
                         JSObject ret = new JSObject();
                         ret.put("hidden", (uiOptions & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
                         call.resolve(ret);
